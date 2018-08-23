de57062146492b1e9cf68587053947838a637c6d
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-08-23 11:37:45 -0400

    followed tutorial to create a simple dynamic todo list using react

---
+++ src/ListItem.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MyList extends Component {
+  render() {
+    return (
+      <li>{this.props.doThis}</li>
+    );
+  }
+}
+
+export default MyList;
